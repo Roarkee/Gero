a35ff3a9c8308717cb7dca7da7341901d23662b3
Roarkee <henokammaccarthy10@gmail.com>
2025-08-12 16:44:51 +0000

    super user issue

--- backend/users/models.py
+++ backend/users/models.py
@@ -18,6 +18,7 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
@@ -32,6 +33,7 @@ class User(AbstractBaseUser):
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     password = models.CharField(max_length=128)
