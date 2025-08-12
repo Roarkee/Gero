4c8084bc7bdb82364d13d116fea14b97b2f8053f
Leslie <lesliedarko18@gmail.com>
2025-08-12 17:20:47 +0000

    fixed urls

--- backend/client/urls.py
+++ backend/client/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from client.views import ClientView, ClientDetailView, ClientOverviewView
 
 urlpatterns =[
-    path('clients/', ClientView.as_view(), name='client-list'),
-    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
-    path('clients/<int:pk>/overview/', ClientOverviewView.as_view(), name='client-overview'),
+    path('', ClientView.as_view(), name='client-list'),
+    path('<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
+    path('<int:pk>/overview/', ClientOverviewView.as_view(), name='client-overview'),
 ]
